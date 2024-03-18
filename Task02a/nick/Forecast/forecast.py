69988e8ff4909704a95694a844c84fdf9ef9f95a
Nicholas Syrotiuk <sefnyn@gmail.com>
2024-03-18 14:02:20 +0000

    tidied code

--- Task02a/nick/Forecast/forecast.py
+++ Task02a/nick/Forecast/forecast.py
@@ -33,11 +33,9 @@ class CancelledError(Exception):pass
 WEATHER_API  =  'https://api.met.no/weatherapi/locationforecast/2.0/compact'
 headers_met  = {}
 headers_met['User-Agent'] =  'Testing out forecasting API'
-
 coordinates = {}
 
-# Prompt user for location
-while True:
+def get_location():    
     try:
         print("\nGlobal weather forecasting system")
         print(  "---------------------------------")
@@ -52,44 +50,50 @@ while True:
         headers_geo["Accept"] = "application/json"
         response_geo = requests.get(url_geo, headers=headers_geo)
         print(response_geo.status_code)
-        try:
-            formatted_location = response_geo.json()['features'][0]['properties']['formatted']
-            geo_timezone = response_geo.json()['features'][0]['properties']['timezone']['name']
-            coordinates['lon'] = response_geo.json()['features'][0]['bbox'][0]
-            coordinates['lat'] = response_geo.json()['features'][0]['bbox'][1]
-
-            print(f"Call weather API with coordinates {coordinates}")
-            response_met  =  requests.get(WEATHER_API, headers=headers_met, params=coordinates)
-            print(response_met.status_code)
-
-            forecast  =  response_met.json()['properties']['timeseries'][0]
-        #    print(forecast)
-            time  = pendulum.parse(forecast['time'])
-            localtime  =  time.in_timezone(geo_timezone) # You can change this to your local timezone see 'TZ identifier' here: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
-            celsius    =  forecast['data']['instant']['details']['air_temperature']
-            fahrenheit = (celsius  *  9/5) +  32
-            humidity   = forecast['data']['instant']['details']['relative_humidity'] 
-            windspeed  = forecast['data']['instant']['details']['wind_speed']
-            symbol1    = forecast['data']['next_1_hours']['summary']['symbol_code']
-            symbol6    = forecast['data']['next_6_hours']['summary']['symbol_code']
-
-            print(f"\nLocation is: {formatted_location}")
-            print(f"Local timezone is: {geo_timezone}")
-            print(f"Local time to nearest hour: {localtime.format('dddd Do [of] MMMM YYYY h:mm A')}")
-            print("Forecast is:")
-            print(f"  Air temperature: {celsius} degrees Celsius ({round(fahrenheit, 2)} degrees Fahrenheit)")
-            print(f"  Relative humidity: {humidity} %")
-            print(f"  Wind speed: {windspeed} m/s")
-            print(f"  Next hour: {symbol1}")
-            print(f"  Next six hours: {symbol6}")
-
-        except IndexError:
-            print(f"Did not find location {location} in the Geocoding API database")
-
+        return(response_geo)
     except CancelledError:
         pass
 
+def get_forecast(response):
+    try:
+        formatted_location = response.json()['features'][0]['properties']['formatted']
+        geo_timezone = response.json()['features'][0]['properties']['timezone']['name']
+        coordinates['lon'] = response.json()['features'][0]['bbox'][0]
+        coordinates['lat'] = response.json()['features'][0]['bbox'][1]
+
+        print(f"Call weather API with coordinates {coordinates}")
+        response_met  =  requests.get(WEATHER_API, headers=headers_met, params=coordinates)
+        print(response_met.status_code)
+
+        forecast  =  response_met.json()['properties']['timeseries'][0]
+        #    print(forecast)
+        time  = pendulum.parse(forecast['time'])
+        localtime  =  time.in_timezone(geo_timezone) # You can change this to your local timezone see 'TZ identifier' here: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+        celsius    =  forecast['data']['instant']['details']['air_temperature']
+        fahrenheit = (celsius  *  9/5) +  32
+        humidity   = forecast['data']['instant']['details']['relative_humidity'] 
+        windspeed  = forecast['data']['instant']['details']['wind_speed']
+        symbol1    = forecast['data']['next_1_hours']['summary']['symbol_code']
+        symbol6    = forecast['data']['next_6_hours']['summary']['symbol_code']
+
+        print(f"\nLocation is: {formatted_location}")
+        print(f"Local timezone is: {geo_timezone}")
+        print(f"Local time to nearest hour: {localtime.format('dddd Do [of] MMMM YYYY h:mm A')}")
+        print("Current forecast is:")
+        print(f"  Air temperature: {celsius} degrees Celsius ({round(fahrenheit, 2)} degrees Fahrenheit)")
+        print(f"  Relative humidity: {humidity} %")
+        print(f"  Wind speed: {windspeed} m/s")
+        print(f"  Next hour: {symbol1}")
+        print(f"  Next six hours: {symbol6}")
+
+    except IndexError:
+        print(f"Did not find location {location} in the Geocoding API database")
+
+while True:
+    r = get_location()
+    get_forecast(r)
     if (getstring.get("\nFetch another forecast?  (y/n)", default="y").lower() not in {"y", "yes"}):
         print("Thank you for using the Global weather forecasting system!")
         break
-#EOF
+
+    #[EOF]
